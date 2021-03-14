b680de729579940a5345142c0bed71c4e07624d3
Dmitry Kartesv <dmitry.kartsev@cnvrg.io>
2021-03-14 10:59:21 +0200

    adding redis

---
+++ pkg/redis/redis.go
@@ -0,0 +1,55 @@
+package redis
+
+import (
+	mlopsv1 "github.com/cnvrg-operator/api/v1"
+	"github.com/cnvrg-operator/pkg/desired"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+const path = "/pkg/redis/tmpl"
+
+var state = []*desired.State{
+	{
+		Name:           "",
+		TemplatePath:   path + "/pvc.tpl",
+		Template:       nil,
+		ParsedTemplate: "",
+		Obj:            &unstructured.Unstructured{},
+		GVR:            desired.Kinds[desired.PvcGVR],
+		Own:            true,
+	},
+	{
+		Name:           "",
+		TemplatePath:   path + "/svc.tpl",
+		Template:       nil,
+		ParsedTemplate: "",
+		Obj:            &unstructured.Unstructured{},
+		GVR:            desired.Kinds[desired.SvcGVR],
+		Own:            true,
+	},
+	{
+		Name:           "",
+		TemplatePath:   path + "/cm.tpl",
+		Template:       nil,
+		ParsedTemplate: "",
+		Obj:            &unstructured.Unstructured{},
+		GVR:            desired.Kinds[desired.ConfigMapGVR],
+		Own:            true,
+	},
+	{
+		Name:           "",
+		TemplatePath:   path + "/dep.tpl",
+		Template:       nil,
+		ParsedTemplate: "",
+		Obj:            &unstructured.Unstructured{},
+		GVR:            desired.Kinds[desired.DeploymentGVR],
+		Own:            true,
+	},
+}
+
+func State(cnvrgApp *mlopsv1.CnvrgApp) []*desired.State {
+	if cnvrgApp.Spec.Redis.Enabled == "true" {
+		return state
+	}
+	return nil
+}
