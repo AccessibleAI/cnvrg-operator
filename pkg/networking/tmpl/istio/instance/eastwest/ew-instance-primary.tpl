nctiggy/cnvrg-operator-1

Struct change + custom ips for eastwest

pkg/networking/tmpl/istio/instance/eastwest/ew-instance-primary.tpl
@@ -48,7 +48,7 @@ spec:
           {{ $name }}: "{{ $value }}"
         {{- end }}
         hpaSpec:
-          maxReplicas: 5
+          maxReplicas: 20
           metrics:
             - resource:
                 name: cpu
@@ -61,8 +61,8 @@ spec:
             name: cnvrg-ingressgateway
         resources:
           limits:
-            cpu: 2000m
-            memory: 1024Mi
+            cpu: 2
+            memory: 4G
           requests:
             cpu: 100m
             memory: 128Mi
@@ -110,6 +110,25 @@ spec:
             value: "{{ .Spec.Tenancy.Value }}"
             effect: "NoSchedule"
         {{- end }}
+        hpaSpec:
+          maxReplicas: 20
+          metrics:
+            - resource:
+                name: cpu
+                targetAverageUtilization: 80
+              type: Resource
+          minReplicas: 1
+          scaleTargetRef:
+            apiVersion: apps/v1
+            kind: Deployment
+            name: isiod
+        resources:
+          limits:
+            cpu: 2
+            memory: 4G
+          requests:
+            cpu: 100m
+            memory: 128Mi
   values:
     global:
       {{- if (gt (len .Spec.Networking.EastWest.RemoteClusters) 0) }}

pkg/networking/tmpl/istio/instance/eastwest/ew-instance-remote.tpl
@@ -55,7 +55,7 @@ spec:
           {{ $name }}: "{{ $value }}"
         {{- end }}
         hpaSpec:
-          maxReplicas: 5
+          maxReplicas: 20
           metrics:
             - resource:
                 name: cpu
@@ -68,8 +68,8 @@ spec:
             name: cnvrg-eastwestgateway
         resources:
           limits:
-            cpu: 2000m
-            memory: 1024Mi
+            cpu: 2
+            memory: 4G
           requests:
             cpu: 100m
             memory: 128Mi
@@ -123,6 +123,25 @@ spec:
             value: "{{ .Spec.Tenancy.Value }}"
             effect: "NoSchedule"
         {{- end }}
+        hpaSpec:
+          maxReplicas: 20
+          metrics:
+            - resource:
+                name: cpu
+                targetAverageUtilization: 80
+              type: Resource
+          minReplicas: 1
+          scaleTargetRef:
+            apiVersion: apps/v1
+            kind: Deployment
+            name: isiod
+        resources:
+          limits:
+            cpu: 2
+            memory: 4G
+          requests:
+            cpu: 100m
+            memory: 128Mi
   values:
     global:
       istioNamespace:  {{ ns . }}

pkg/networking/tmpl/istio/instance/instance.tpl
@@ -48,7 +48,7 @@ spec:
           {{ $name }}: "{{ $value }}"
         {{- end }}
         hpaSpec:
-          maxReplicas: 5
+          maxReplicas: 20
           metrics:
             - resource:
                 name: cpu
@@ -61,8 +61,8 @@ spec:
             name: cnvrg-ingressgateway
         resources:
           limits:
-            cpu: 2000m
-            memory: 1024Mi
+            cpu: 2
+            memory: 4G
           requests:
             cpu: 100m
             memory: 128Mi
@@ -110,6 +110,25 @@ spec:
             value: "{{ .Spec.Tenancy.Value }}"
             effect: "NoSchedule"
         {{- end }}
+        hpaSpec:
+          maxReplicas: 20
+          metrics:
+            - resource:
+                name: cpu
+                targetAverageUtilization: 80
+              type: Resource
+          minReplicas: 1
+          scaleTargetRef:
+            apiVersion: apps/v1
+            kind: Deployment
+            name: isiod
+        resources:
+          limits:
+            cpu: 2
+            memory: 4G
+          requests:
+            cpu: 100m
+            memory: 128Mi
   values:
     global:
       istioNamespace:  {{ ns . }}

AccessibleAI/cnvrg-operator

pkg/networking/tmpl/istio/instance/eastwest/ew-instance-primary.tpl
@@ -48,7 +48,7 @@ spec:
           {{ $name }}: "{{ $value }}"
         {{- end }}
         hpaSpec:
-          maxReplicas: 5
+          maxReplicas: 20
           metrics:
             - resource:
                 name: cpu
@@ -61,8 +61,8 @@ spec:
             name: cnvrg-ingressgateway
         resources:
           limits:
-            cpu: 2000m
-            memory: 1024Mi
+            cpu: "2"
+            memory: 4G
           requests:
             cpu: 100m
             memory: 128Mi
@@ -110,6 +110,25 @@ spec:
             value: "{{ .Spec.Tenancy.Value }}"
             effect: "NoSchedule"
         {{- end }}
+        hpaSpec:
+          maxReplicas: 20
+          metrics:
+            - resource:
+                name: cpu
+                targetAverageUtilization: 80
+              type: Resource
+          minReplicas: 1
+          scaleTargetRef:
+            apiVersion: apps/v1
+            kind: Deployment
+            name: istiod
+        resources:
+          limits:
+            cpu: "2"
+            memory: 4G
+          requests:
+            cpu: 100m
+            memory: 128Mi
   values:
     global:
       {{- if (gt (len .Spec.Networking.EastWest.RemoteClusters) 0) }}

pkg/networking/tmpl/istio/instance/eastwest/ew-instance-remote.tpl
@@ -55,7 +55,7 @@ spec:
           {{ $name }}: "{{ $value }}"
         {{- end }}
         hpaSpec:
-          maxReplicas: 5
+          maxReplicas: 20
           metrics:
             - resource:
                 name: cpu
@@ -68,8 +68,8 @@ spec:
             name: cnvrg-eastwestgateway
         resources:
           limits:
-            cpu: 2000m
-            memory: 1024Mi
+            cpu: "2"
+            memory: 4G
           requests:
             cpu: 100m
             memory: 128Mi
@@ -123,6 +123,25 @@ spec:
             value: "{{ .Spec.Tenancy.Value }}"
             effect: "NoSchedule"
         {{- end }}
+        hpaSpec:
+          maxReplicas: 20
+          metrics:
+            - resource:
+                name: cpu
+                targetAverageUtilization: 80
+              type: Resource
+          minReplicas: 1
+          scaleTargetRef:
+            apiVersion: apps/v1
+            kind: Deployment
+            name: istiod
+        resources:
+          limits:
+            cpu: "2"
+            memory: 4G
+          requests:
+            cpu: 100m
+            memory: 128Mi
   values:
     global:
       istioNamespace:  {{ ns . }}

pkg/networking/tmpl/istio/instance/instance.tpl
@@ -48,7 +48,7 @@ spec:
           {{ $name }}: "{{ $value }}"
         {{- end }}
         hpaSpec:
-          maxReplicas: 5
+          maxReplicas: 20
           metrics:
             - resource:
                 name: cpu
@@ -61,8 +61,8 @@ spec:
             name: cnvrg-ingressgateway
         resources:
           limits:
-            cpu: 2000m
-            memory: 1024Mi
+            cpu: "2"
+            memory: 4G
           requests:
             cpu: 100m
             memory: 128Mi
@@ -110,6 +110,25 @@ spec:
             value: "{{ .Spec.Tenancy.Value }}"
             effect: "NoSchedule"
         {{- end }}
+        hpaSpec:
+          maxReplicas: 20
+          metrics:
+            - resource:
+                name: cpu
+                targetAverageUtilization: 80
+              type: Resource
+          minReplicas: 1
+          scaleTargetRef:
+            apiVersion: apps/v1
+            kind: Deployment
+            name: istiod
+        resources:
+          limits:
+            cpu: "2"
+            memory: 4G
+          requests:
+            cpu: 100m
+            memory: 128Mi
   values:
     global:
       istioNamespace:  {{ ns . }}