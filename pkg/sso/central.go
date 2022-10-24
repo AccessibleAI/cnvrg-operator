ec96b60091d21762d297b3b26bd21c7abd111cd8
Dmitry Kartsev <dmitry.kartsev@cnvrg.io>
2022-10-24 09:24:42 +0300

    improve jwksUrlWithAudience, update docs

--- pkg/sso/central.go
+++ pkg/sso/central.go
@@ -121,11 +121,7 @@ func (c *CentralStateManager) jwksUrl() string {
 
 func (c *CentralStateManager) jwksUrlWithAudience() string {
 
-	return fmt.Sprintf("%s://%s.%s/v1/%s/.well-known/jwks.json?client_id=cnvrg-tenant",
-		c.schema(),
-		c.app.Spec.SSO.Jwks.Name,
-		c.app.Spec.ClusterDomain,
-		c.domainId())
+	return fmt.Sprintf("%s=cnvrg-tenant", c.jwksUrl())
 }
 
 func (c *CentralStateManager) Apply() error {
