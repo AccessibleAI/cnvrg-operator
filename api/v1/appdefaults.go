614f6a4fb0d125d5c73fb5deb32b3cab785ae0ef
Dmitry Kartsev <dmitry.kartsev@cnvrg.io>
2022-12-07 12:20:01 +0200

    upgrade cnvrg proxy version

--- api/v1/appdefaults.go
+++ api/v1/appdefaults.go
@@ -473,7 +473,7 @@ var ssoDefault = SSO{
 		Enabled:                          false,
 		Replicas:                         1,
 		SvcName:                          "sso-central",
-		CnvrgProxyImage:                  "cnvrg-proxy:v1.0.6",
+		CnvrgProxyImage:                  "cnvrg-proxy:v1.0.7",
 		OauthProxyImage:                  "oauth2-proxy:v7.3.x.ssov3.p2-01",
 		CentralUiImage:                   "centralsso:latest",
 		EmailDomain:                      []string{"*"},
@@ -494,7 +494,7 @@ var ssoDefault = SSO{
 	Proxy: CentralProxy{
 		Enabled:  false,
 		Replicas: 1,
-		Image:    "cnvrg-proxy:v1.0.6",
+		Image:    "cnvrg-proxy:v1.0.7",
 		SvcName:  "cnvrg-proxy-central",
 		Requests: Requests{
 			Cpu:    "200m",
