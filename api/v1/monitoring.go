996c4cdb3349ed21ece834ca4b71f67e7fd9b925
Brad Soper <brad.soper@gmail.com>
2023-03-07 15:09:19 -0800

    updated the cr template to provide correct permissions, updated the image to the latest for kube state metrics 2.8.1

--- api/v1/monitoring.go
+++ api/v1/monitoring.go
@@ -179,7 +179,7 @@ var infraMonitoringDefault = CnvrgInfraMonitoring{
 	},
 	KubeStateMetrics: KubeStateMetrics{
 		Enabled: false,
-		Image:   "kube-state-metrics:v1.9.7",
+		Image:   "kube-state-metrics:v2.8.1",
 	},
 	NodeExporter: NodeExporter{
 		Enabled: false,
